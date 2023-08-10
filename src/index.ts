d1e4a568f2d6626c2dc2529c58b680dd0cea0b7c
aaaravv <aaravmishra619@gmail.com>
2023-08-10 09:59:56 +0530

    chore: add basic schema types

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,21 @@
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
 
+import { typeDefs } from "./schema";
+
+/**
+ * Type definitions (schema)
+ * These are the definitions of different types of data that we want to expose on our graph
+ *
+ * Resolvers
+ */
+
 // server setup
+// typeDefs, data types and relationships
+// resolvers, functions that run on each request
+
 const server = new ApolloServer({
-	// typeDefs, data types and relationships
-	// resolvers, functions that run on each request
+	typeDefs,
 });
 
 const { url } = await startStandaloneServer({ server, listen: { port: 4000 } });
