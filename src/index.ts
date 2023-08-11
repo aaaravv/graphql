47bfbaafd7f67e2af9d51c0d8dc2ac4b0f9ae213
aaaravv <aaravmishra619@gmail.com>
2023-08-11 09:18:06 +0530

    chore: add resolvers for exposed nodes on the graph
    to handle nested queries through out the graph

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,66 @@
-import { ApolloServer } from "@apollo/server";
-import { startStandaloneServer } from "@apollo/server/standalone";
+const { ApolloServer } = require("@apollo/server");
+const { startStandaloneServer } = require("@apollo/server/standalone");
 
-import { typeDefs } from "./schema";
+const db = require("./_db");
+const schema = require("./schema");
 
 /**
  * Type definitions (schema)
  * These are the definitions of different types of data that we want to expose on our graph
  *
  * Resolvers
+ * These are the functions that are called when a query is made against our graph
+ * They are responsible for fetching the data for a given type and returning it
  */
 
-// server setup
-// typeDefs, data types and relationships
-// resolvers, functions that run on each request
+const resolvers = {
+	Query: {
+		review: (_: any, args: { id: number }) => {
+			return db.reviews.find((review) => review.id === args.id);
+		},
+		reviews: () => {
+			return db.reviews;
+		},
+
+		game: (_: any, args: { id: number }) => {
+			return db.games.find((game) => game.id === args.id);
+		},
+		games: () => {
+			return db.games;
+		},
+
+		author: (_: any, args: { id: number }) => {
+			return db.authors.find((author) => author.id === args.id);
+		},
+		authors: () => {
+			return db.authors;
+		},
+	},
+	Game: {
+		reviews: (parent: { id: number }) => {
+			return db.reviews.filter((review) => review.game_id === parent.id);
+		},
+	},
+	Review: {
+		Game: (parent: { game_id: number }) => {
+			return db.games.find((game) => game.id === parent.game_id);
+		},
+		Author: (parent: { author_id: number }) => {
+			return db.authors.find((author) => author.id === parent.author_id);
+		},
+	},
+	Author: {
+		reviews: (parent: { id: number }) => {
+			return db.reviews.filter((review) => review.author_id === parent.id);
+		},
+	},
+};
 
 const server = new ApolloServer({
-	typeDefs,
+	typeDefs: schema.typeDefs,
+	resolvers,
 });
 
-const { url } = await startStandaloneServer({ server, listen: { port: 4000 } });
-
-console.log(`🚀 Server ready at ${url}`);
+startStandaloneServer(server, { listen: { port: 4000 } }).then(({ url }) => {
+	console.log(`🚀 Server ready at ${url}`);
+});
--- src/schema.ts
+++ src/schema.ts
@@ -2,26 +2,34 @@
  * Query Type: special type for queries which define the entry point of every GraphQL query
  */
 
-export const typeDefs = `#graphql
+const typeDefs = `#graphql
   type Game {
     id: ID!
     title: String!
     platform: [String!]!
+    reviews: [Review!]
   }
   type Review {
     id: ID!
     rating: Int!
     content: String!
+    Game: Game!
+    Author: Author!
   }
   type Author {
     id: ID!
     name: String!
     verified: Boolean!
+    reviews: [Review!]
   }
   type Query {
+    review(id: ID!): Review
     reviews: [Review]
+    game(id: ID!): Game
     games: [Game]
+    author(id: ID!): Author
     authors: [Author]
   }
-
 `;
+
+module.exports = { typeDefs };
